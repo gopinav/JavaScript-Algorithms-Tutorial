5aa6d1c6f3903e9a896307439d6105d9ef4eb212
Vishwas <vishwas.g5@gmail.com>
2022-03-14 13:29:11 +0530

    Fibonacci sequence

---
+++ math/fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(n) {
+  const fib = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib
+}
+
+console.log(fibonacci(2)) // [0,1]
+console.log(fibonacci(3)) // [0,1,1]
+console.log(fibonacci(7)) // [0,1,1,2,3,5,8]
