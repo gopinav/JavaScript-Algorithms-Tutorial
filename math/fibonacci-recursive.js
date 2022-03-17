5a6fa921fa4703f717ae76f66f18f8293c9e4944
Vishwas <vishwas.g5@gmail.com>
2022-03-17 16:33:45 +0530

    Recursive fibonacci solution

---
+++ math/fibonacci-recursive.js
@@ -0,0 +1,10 @@
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
+}
+
+console.log(recursiveFibonacci(0)) // 0
+console.log(recursiveFibonacci(1)) // 1
+console.log(recursiveFibonacci(7)) // 8
